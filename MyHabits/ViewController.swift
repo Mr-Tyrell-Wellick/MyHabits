20a9225fe945d25b01c8901aae3683667c5ff985
Mr-Tyrell-Wellick <rorshix97@gmail.com>
2022-11-11 16:18:49 +0400

    Initial Commit

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Ульви Пашаев on 11.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
